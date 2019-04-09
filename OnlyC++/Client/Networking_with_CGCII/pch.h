726a4b0aa2ea9cb186cc84ac799b131bbeab5a09
des5141 <rhea31@naver.com>
2019-04-09 19:29:21 +0900

    Client Added

---
+++ OnlyC++/Client/Networking_with_CGCII/pch.h
@@ -0,0 +1,16 @@
+﻿
+#pragma once
+//----------------------------------------------------------------------------
+// Definitions
+//----------------------------------------------------------------------------
+// 1) For gcc
+#undef __STRICT_ANSI__
+
+
+// 3) 거의 사용되지 않는 내용은 Windows 헤더에서 제외합니다(WIN32용)
+#define WIN32_LEAN_AND_MEAN		// 거의 사용되지 않는 내용은 Windows 헤더에서 제외합니다.
+
+// 4) Standard Libraries
+#include <stdio.h>
+#include <tchar.h>
+#include <conio.h>
---
+++ OnlyC++/Server/Networking_with_CGCII/Networking_with_CGCII.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include "../../../CGCII/Include/CGNetSocketTemplates.h"
+
+
+class CSocket : public CGNet::Socket::CTCP<>
+{
+	virtual void OnConnect(CGNet::IO::IConnective*) override
+	{
+		printf("Connected\n");
+	}
+
+	virtual void OnDisconnect(uint32_t) override
+	{
+		printf("Disconnected\n");
+	}
+
+	virtual int OnMessage(CGMSG& /*_Msg*/) override
+	{
+		printf("Message Received\n");
+
+		return	0;
+	}
+};
+
+int _tmain(int, _TCHAR*[])
+{
+	auto	pacceptor = NEW<CGNet::CAcceptor<CSocket>>();
+	pacceptor->Start(65535);
+	printf("Server Opened\n");
+	while (_getch() != 27);
+	return 0;
+}
